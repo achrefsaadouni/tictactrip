b00f931ab61813e9b6769b477d78b2183b335bd9
Saadouni Achref <achref.saadouni@esprit.tn>
2019-12-13 17:13:21 +0100

    fixing port

--- app.js
+++ app.js
@@ -126,6 +126,22 @@ app.post('/api/justify',   (req,res,next) => {
 
 
 
+
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+    return false;
+};
+
 const port = normalizePort(process.env.PORT || "2500");
 app.listen(port, function () {
     console.log('Server listening on port 4000!');
